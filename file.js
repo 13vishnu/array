aa6ac531cc985466548830a964dc521a0eb0fc76
13vishnu <114495087+13vishnu@users.noreply.github.com>
2022-10-06 11:58:07 +0100

    array

---
+++ file.js
@@ -0,0 +1,12 @@
+var numbers=[]
+var coloumns=3
+var row=3
+for(let i=0; i<row;i++){
+    numbers [i]=[];
+    for(let j=0;j<coloumns; j++){
+        numbers[i] [j]=j+1;
+    }
+}
+console.log(numbers)
+
+
